bb8793992a4ce2027ed15c2acaa18c5dff6f4f6e
AprilGreen42 <aprilgreen042@gmail.com>
2024-06-17 20:06:15 +0400

    Initial Commit

---
+++ RandomFacts/RandomFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomFactsApp.swift
+//  RandomFacts
+//
+//  Created by Дмитрий on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct RandomFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
